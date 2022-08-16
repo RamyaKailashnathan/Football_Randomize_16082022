fd56d76895559572e6aaca1d65229430be09d9d7
Tec <Tec@SKAB1-PC-01>
2022-08-16 11:09:09 +0200

    Add project files.

---
+++ Football_Randomize_16082022/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Football_Randomize_16082022
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        static player()
+        {
+
+        }
+        static goalKeeper()
+        {
+
+        }
+    }
+}
